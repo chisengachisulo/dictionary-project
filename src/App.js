2d313e3642b1ee5837d7376e30f3adb5ff25ebfe
chisengachisulo <128135390+chisengachisulo@users.noreply.github.com>
2023-12-25 11:04:36 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello</header>
+      <button  className="btn btn-primary">
+         Button
+      </button>
+    </div>
+  );
+}
+
+export default App;
